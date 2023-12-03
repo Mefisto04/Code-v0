a8d4f5bddc8556c87c10214c82e9aba295a11f5b
Mefisto04 <kawalemayur30@gmail.com>
2023-12-03 10:20:59 +0530

    Add Find the Peaks

--- Find_the_Peaks.cpp
+++ Find_the_Peaks.cpp
@@ -3,20 +3,22 @@ public:
     vector<int> findPeaks(vector<int>& arr) {
         vector<int> peaks;
     int n = arr.size();
-    if (n < 3) {
-        return peaks;
-    }
-    if (arr[0] > arr[1]) {
+    if (n == 1) {
         peaks.push_back(0);
-    }
-    for (int i = 1; i < n - 1; i++) {
-        if (arr[i] > arr[i - 1] && arr[i] > arr[i + 1]) {
-            peaks.push_back(i);
+    } else {
+        if (arr[0] >= arr[1]) {
+            peaks.push_back(0);
+        }
+        if (arr[n - 1] >= arr[n - 2]) {
+            peaks.push_back(n - 1);
+        }
+        for (int i = 1; i < n - 1; i++) {
+            if (arr[i] >= arr[i - 1] && arr[i] >= arr[i + 1]) {
+                peaks.push_back(i);
+            }
         }
     }
-    if (arr[n - 1] > arr[n - 2]) {
-        peaks.push_back(n - 1);
-    }
     return peaks;
+
     }
 };
