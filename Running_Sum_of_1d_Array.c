b5f9b40d5f21c3e4c53ccf17964be96ed54da0f7
Mefisto04 <kawalemayur30@gmail.com>
2023-12-04 18:44:39 +0530

    Add Running Sum of 1d Array

---
+++ Running_Sum_of_1d_Array.c
@@ -0,0 +1,13 @@
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* runningSum(int* nums, int numsSize, int* returnSize){
+    int *result = malloc(sizeof(int) * numsSize);
+    *returnSize = numsSize;
+    int sum = 0;
+    for (int i = 0; i < numsSize; i++) {
+        sum += nums[i];
+        result[i] = sum;
+    }
+    return result;
+}
