efe5ac16c4a34c4d59a840ec0d2e33c9bf7899fa
Mefisto04 <kawalemayur30@gmail.com>
2023-12-09 15:01:31 +0530

    Add Remove Adjacent Almost-Equal Characters

---
+++ Remove_Adjacent_Almost-Equal_Characters.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int removeAlmostEqualCharacters(string word) {
+        int temp = 0;
+        for (int i = 1; i < word.size(); ++i) {
+            if (word[i] == word[i - 1] || abs(word[i] - word[i - 1]) == 1) {
+                temp++;
+                i++;
+            }
+        }
+        return temp; 
+    }
+};
