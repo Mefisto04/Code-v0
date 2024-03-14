1fb0f5d9b53e654346cc1ac7a6dcbeca996a9d19
Mefisto04 <kawalemayur30@gmail.com>
2024-03-14 17:24:07 +0530

    Add Binary Subarrays With Sum

---
+++ Binary_Subarrays_With_Sum.cpp
@@ -0,0 +1,30 @@
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int numSubarraysWithSum(vector<int>& nums, int goal) {
+        int count = 0;
+        int sum = 0;
+        int left = 0;
+        int right = 0;
+
+        while (right < nums.size()) {
+            sum += nums[right];
+            while (left < right && sum > goal) {
+                sum -= nums[left++];
+            }
+            if (sum == goal) {
+                int temp = left;
+                while (temp < right && nums[temp] == 0) {
+                    count++;
+                    temp++;
+                }
+                count++;
+            }
+            right++;
+        }
+
+        return count;
+    }
+};
