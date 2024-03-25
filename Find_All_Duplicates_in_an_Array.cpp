851c2bd429eec76066016e91932f01de243f104d
Mefisto04 <kawalemayur30@gmail.com>
2024-03-25 13:05:09 +0530

    Add Find All Duplicates in an Array

--- Find_All_Duplicates_in_an_Array.cpp
+++ Find_All_Duplicates_in_an_Array.cpp
@@ -1,14 +1,13 @@
 class Solution {
 public:
     vector<int> findDuplicates(vector<int>& v) {
-        sort(v.begin(),v.end());
-        int n=v.size();
+        map<int,int>m;
+        for(auto i:v)
+            m[i]++;
         vector<int>ans;
-        for(int i=0;i<n-1;i++) {
-            if(v[i]==v[i+1]) {
-                ans.push_back(v[i]);
-                i++;
-            }
+        for(auto i:m) {
+            if(i.second==2)
+                ans.push_back(i.first);
         }
         return ans;
     }
