fe16af4d676a5488ff8dbca937a1e4704d51e471
Mefisto04 <kawalemayur30@gmail.com>
2024-04-05 18:41:08 +0530

    Add Make The String Great

--- Make_The_String_Great.cpp
+++ Make_The_String_Great.cpp
@@ -5,7 +5,7 @@ public:
         string ans=string(1, s[0]);
         for(int i=1; i<n; i++){
             if (!ans.empty() && s[i]!=ans.back() 
-            && (s[i]&31)==(ans.back()&31))
+            && (s[i]&31)==(ans.back()&31))//Note 'a'='A'+32 1-bit different
                 ans.pop_back();
             else 
                 ans.push_back(s[i]);
