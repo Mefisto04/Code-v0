4f485d1ca329d9c080be30fa7106e1f290f9678e
Mefisto04 <kawalemayur30@gmail.com>
2024-06-12 21:22:16 +0530

    Add Find Triangular Sum of an Array

--- Find_Triangular_Sum_of_an_Array.cpp
+++ Find_Triangular_Sum_of_an_Array.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
     int triangularSum(vector<int>& nums) {
-        for(int i=1;i<nums.size();i++){
-            for(int j=0;j<nums.size()-1;j++){
+        for(int i=1;i<nums.length;i++){
+            for(int j=0;j<nums.length-1;j++){
                 nums[j]=(nums[j]+nums[j+1])%10;
             }
         }
