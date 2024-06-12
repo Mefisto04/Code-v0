abc9c699f1707e4eded328af1cc61c4b4ad87351
Mefisto04 <kawalemayur30@gmail.com>
2024-06-12 13:05:10 +0530

    Add Sort Colors

--- Sort_Colors.cpp
+++ Sort_Colors.cpp
@@ -1,26 +1,14 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        int s=0, m=0;
-        int e=nums.size()-1;
-        while(m<=e)
-        {
-            if(nums[m]==0)
-            {
-                swap(nums[s],nums[m]);
-                m++;
-                s++;
-            }
-           else if(nums[m]==1)
-            {
-                m--;
-            }
-            else if(nums[m]==2)
-            {
-                swap(nums[m],nums[e]);
-                e++;
-            }
+        int freq[3]={0};
+        for(int x: nums)
+            freq[x]++;
+        int n=nums.size(), count=0;
+        for(int x=0; x<3; x++){
+            fill(nums.begin()+count, nums.begin()+count+freq[x], x);
+            count+=freq[x];
         }
-
+        
     }
 };
