df128c5822c38cd5b1d598765990c6176363b949
Mefisto04 <kawalemayur30@gmail.com>
2024-06-14 07:33:17 +0530

    Add Minimum Number of Moves to Seat Everyone

--- Minimum_Number_of_Moves_to_Seat_Everyone.cpp
+++ Minimum_Number_of_Moves_to_Seat_Everyone.cpp
@@ -1,14 +1,12 @@
 class Solution {
 public:
     int minMovesToSeat(vector<int>& seats, vector<int>& students) {
-        int n = seats.size();
         sort(seats.begin(),seats.end());
         sort(students.begin(),students.end());
-        int result = 0;
-        for(int i=0;i<n;i++){
-            result += abs(seats[i]-students[i]);
-            
+        int ans = 0;
+        for(int i=0;i<seats.size();i++){
+            ans+=abs(seats[i]-students[i]);
         }
-        return result;
+        return ans;
     }
 };
