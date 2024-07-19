3bf361ea4e2aaa1d2ad993ec2592bdeaf5ddeef9
Mefisto04 <kawalemayur30@gmail.com>
2024-07-19 19:07:56 +0530

    Add Sort the People

--- Sort_the_People.cpp
+++ Sort_the_People.cpp
@@ -2,14 +2,18 @@ class Solution{
 public:
     vector<string> sortPeople(vector<string>& names, vector<int>& heights) {
         int n = names.size();
-        unsorted_map<int,string> p;
-        for(int i=0;i<n;i++){
-            p[heights[i]]=names[i];
+        pair<int, string> p[n];
+        for(int i = 0; i < n; i++){
+            p[i].first = heights[i];
+            p[i].second = names[i];
         }
-        sort(heights.begin(),heights.end());
-        vector<string> sorted(n);
-        for(int i=n-1;i>=0;i--){
-            sorted[n-i-1]=p[heights[i]];
-        }
-        return sorted;
+        p.sort([](const auto& a, const auto& b) { 
+        return a.first 
+               < b.first; 
+    }); 
+        vector<string> ans;
+        for(int i = 0; i < n; i++)
+            ans.push_back(p[i].second);
+        return ans;
+    }
 };
