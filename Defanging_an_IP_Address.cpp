c8d500fc2a0dd81453fd9fa81fcba97e95768b28
Mefisto04 <kawalemayur30@gmail.com>
2025-01-07 16:55:30 +0530

    Add Defanging an IP Address

---
+++ Defanging_an_IP_Address.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    string defangIPaddr(string address) {
+        string str = "";
+        int n = address.size();
+        for(int i=0;i<n;i++){
+            if(address[i]=='.') str += "[.]";
+            else str += address[i];
+        }
+        return str;
+    }
+};
