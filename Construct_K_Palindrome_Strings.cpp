702a50837ca77b975f3ca0a19baa22be6e6130d0
Mefisto04 <kawalemayur30@gmail.com>
2025-01-11 12:38:37 +0530

    Add Construct K Palindrome Strings

--- Construct_K_Palindrome_Strings.cpp
+++ Construct_K_Palindrome_Strings.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     bool canConstruct(string s, int k) {
-        if (s.length() < k) return false;
+        
         sort(s.begin(), s.end());
         int oddCount = 0;
         for (int i = 0; i < s.length(); ) {
