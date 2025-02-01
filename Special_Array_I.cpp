122658d61a6af68772224abc451e21e1dc27af77
Mefisto04 <kawalemayur30@gmail.com>
2025-02-01 16:30:29 +0530

    Add Special Array I

--- Special_Array_I.cpp
+++ Special_Array_I.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     bool isArraySpecial(vector<int>& nums) {
         for (int i = 0; i < nums.size() - 1; ++i) {
-            if ((nums[i] + nums[i + 1]) % 2 == 0) return false;
+            if ((nums[i] + nums[i + 1]) % 2 == 0) return true;
         }
         return true;
     }
