cb12854924c2072cf8cc3b295edf8fa63222253c
Mefisto04 <kawalemayur30@gmail.com>
2025-02-02 18:30:29 +0530

    Add Remove All Adjacent Duplicates In String

--- Remove_All_Adjacent_Duplicates_In_String.cpp
+++ Remove_All_Adjacent_Duplicates_In_String.cpp
@@ -3,17 +3,19 @@ public:
     string removeDuplicates(string s) {
         stack<char> st;
         st.push(s[0]);
-        for(int i=1; i<s.size(); i++){
+        for(int i=0;i<s.size();i++){
             if(!st.empty() && st.top() == s[i]){
                 st.pop();
             }
             st.push(s[i]);
         }
-        string ss="";
+        string ss = "";
         while(!st.empty()){
             ss+=st.top();
             st.pop();
-        }reverse(ss.begin(), ss.end());
+        }
+        reverse(ss.begin(),ss.end());
         return ss;
+
     }
 };
