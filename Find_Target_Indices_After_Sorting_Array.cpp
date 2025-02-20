1a5c0f3e1828e1a9e1b71095b6eec5690bb3cad3
Mefisto04 <kawalemayur30@gmail.com>
2025-02-20 19:40:01 +0530

    Add Find Target Indices After Sorting Array

--- Find_Target_Indices_After_Sorting_Array.cpp
+++ Find_Target_Indices_After_Sorting_Array.cpp
@@ -1,31 +1,13 @@
 class Solution {
 public:
     vector<int> targetIndices(vector<int>& nums, int target) {
-        int small = 0,greater = 0, check = 0;
-        int n = nums.size();
-        for(int i = 0 ; i < n ; i++){
-            if(nums[i] == target){
-                check++;
+        vector<int>ans;
+        sort(nums.begin(),nums.end());
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]==target){
+                ans.push_back(i);
             }
         }
-        vector<int> result(check);
-        if(check == 0) {
-            return result;
-        }
-        for(int i = 0 ; i < n ; i++) {
-            if(nums[i] > target){
-                greater++;
-            }
-            else{
-                small++;
-            }
-        }
-        small = small-check;
-        for(int i = 0 ; i < check ; i++){
-            result[i] = small ;
-            small++;
-        }
-        return result;
-        
+        return ans;
     }
 };
