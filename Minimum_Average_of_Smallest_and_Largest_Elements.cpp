2d75b0c7af70ec115a7449bd498f4516c5b023e6
Mefisto04 <kawalemayur30@gmail.com>
2025-02-25 17:30:45 +0530

    Add Minimum Average of Smallest and Largest Elements

---
+++ Minimum_Average_of_Smallest_and_Largest_Elements.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    double minimumAverage(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        int n = nums.size();
+        vector<double>ans;
+        int left = 0;
+        int right = n-1;
+        while(left<right){
+            double avg = (nums[right]+nums[left])/2;
+            ans.push_back(avg);
+            left++;
+            right--;
+        }
+        sort(ans.begin(),ans.end());
+        return ans[0];
+    }
+};
