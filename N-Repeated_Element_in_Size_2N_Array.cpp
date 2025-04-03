3844219959b11a7d80c02c6de90fd7da2191296b
Mefisto04 <kawalemayur30@gmail.com>
2025-04-03 12:56:32 +0530

    Add N-Repeated Element in Size 2N Array

--- N-Repeated_Element_in_Size_2N_Array.cpp
+++ N-Repeated_Element_in_Size_2N_Array.cpp
@@ -1,12 +1,11 @@
 class Solution {
 public:
-    int repeatedNTimes(vector<int>& nums) {
+    int repeatedNTimes2(vector<int>& A) {
         unordered_set<int> seen;
         for (int a: A) {
             if (seen.count(a))
                 return a;
             seen.insert(a);
         }
-
     }
 };
