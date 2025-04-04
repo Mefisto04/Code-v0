a110cee4ab35a9909fd56770cea5627484c4227a
Mefisto04 <kawalemayur30@gmail.com>
2025-04-04 14:10:10 +0530

    Add The kth Factor of n

--- The_kth_Factor_of_n.cpp
+++ The_kth_Factor_of_n.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int kthFactor(int n, int k) {
-        vector<int>a;
+        vector<int>arr;
         for(int i=1;i<=n;i++){
             if(n%i==0) a.push_back(i);
         }
