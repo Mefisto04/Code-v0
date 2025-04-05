21cc9b8d5c077715b3ebb95d3876081669e63641
Mefisto04 <kawalemayur30@gmail.com>
2025-04-05 19:29:41 +0530

    Add Two Sum

--- Two_Sum.cpp
+++ Two_Sum.cpp
@@ -1,17 +1,13 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        int n = nums.size();
-        vector<int>arr;
-        for(int i=0;i<n;i++){
-            for(int j=i+1;j<n;j++){
-                if(nums[i]+nums[j]==target){
-                    arr.push_back(i);
-                    arr.push_back(j);
-                    break;
-                }
-            }
+        unordered_map<int, int> seen;
+        for (int i = 0; i < nums.size(); i++) {
+            int complement = target - nums[i];
+            if (seen.count(complement)) 
+                return {seen[complement], i};
+            seen[nums[i]] = i;
         }
-        return arr;
+        return {};
     }
 };
