ea452bee67b209ea1f2b8ec8c182a4c89e576eac
Mefisto04 <kawalemayur30@gmail.com>
2025-04-10 18:39:16 +0530

    Add Count the Number of Vowel Strings in Range

--- Count_the_Number_of_Vowel_Strings_in_Range.cpp
+++ Count_the_Number_of_Vowel_Strings_in_Range.cpp
@@ -3,7 +3,7 @@ public:
     int vowelStrings(vector<string>& words, int left, int right) {
         unordered_set<char> vowels = {'a','e','i','o','u'};
         int count = 0 ;
-        for(int i=left;i<=right;i++){
+        for(int i=left;i<right;i++){
             string word = words[i];
             if(vowels.count(word.front()) && vowels.count(word.back())){
                 count++;
