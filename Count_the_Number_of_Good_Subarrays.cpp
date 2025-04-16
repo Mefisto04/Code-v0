3ffdecc3f2f4bcc2dd63b620567da53b38278f40
Mefisto04 <kawalemayur30@gmail.com>
2025-04-16 07:45:30 +0530

    Add Count the Number of Good Subarrays

--- Count_the_Number_of_Good_Subarrays.cpp
+++ Count_the_Number_of_Good_Subarrays.cpp
@@ -1,14 +1,11 @@
 class Solution {
 public:
     long long countGood(vector<int>& nums, int k, long long cnt = 0) {
-        for (int i = 0; i < nums.size(); i++) {
-            unordered_map<int, int> mpp;
-            long long pairs = 0;
-            for (int left = i; left < nums.size(); left++) {
-                pairs += mpp[nums[left]];
-                mpp[nums[left]]++;
-                if (pairs >= k) cnt++;
-            }
+        unordered_map <int,int> mpp;
+        for (int i = 0, left = 0 ; i < nums.size() ; i++) {
+            k -= mpp[nums[i]]++;
+            while (k <= 0) k += --mpp[nums[left++]];
+            cnt += left;
         }
         return cnt;
     }
