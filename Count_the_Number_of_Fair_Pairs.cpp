86b77c5d5d9e0077b91e3da098f48fd5cf580d86
Mefisto04 <kawalemayur30@gmail.com>
2025-04-19 14:38:52 +0530

    Add Count the Number of Fair Pairs

--- Count_the_Number_of_Fair_Pairs.cpp
+++ Count_the_Number_of_Fair_Pairs.cpp
@@ -1,14 +1,10 @@
 class Solution {
     public:
-        long long countFairPairs(vector<int>& nums, int lower, int upper) {
-            long long ans = 0;
-            sort(nums.begin(), nums.end());
-            for (int i = 0; i + 1 < nums.size(); i++) {
-                int minReq = lower - nums[i], maxReq = upper - nums[i];
-                auto high = upper_bound(nums.begin() + i + 1, nums.end(), maxReq);
-                auto low = lower_bound(nums.begin() + i + 1, nums.end(), minReq);
-                ans += (high - low);
-            }
+        long long countFairPairs(vector<int>& nums, int lower, int upper, long long ans = 0) {
+            for(int i = 0 ; i < nums.size(); i++)
+                for(int j = i + 1; j < nums.size(); j++)
+                    if(nums[i] + nums[j] >= lower && nums[i] + nums[j] <= upper) ++ans;
+    
             return ans;
         }
     };
