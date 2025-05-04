9993658de2e81d577c7367a0bf7d2c64483bafee
Mefisto04 <kawalemayur30@gmail.com>
2025-05-04 07:28:19 +0530

    Add Number of Equivalent Domino Pairs

--- Number_of_Equivalent_Domino_Pairs.cpp
+++ Number_of_Equivalent_Domino_Pairs.cpp
@@ -1,14 +1,11 @@
 class Solution {
     public:
         int numEquivDominoPairs(vector<vector<int>>& dominoes) {
+            map<pair<int, int>, int> mpp;
             int count = 0;
-            for (int i = 0 ; i < dominoes.size() ; i++) {
-                for (int j = i + 1 ; j < dominoes.size() ; j++) {
-                    int a = dominoes[i][0], b = dominoes[i][1];
-                    int c = dominoes[j][0], d = dominoes[j][1];
-                    if ((a == c && b == d) || (a == d && b == c))
-                    ++count;
-                }
+            for(auto& d : dominoes){
+                if(d[0] > d[1]) swap(d[0], d[1]);
+                count += mpp[make_pair(d[0], d[1])]++;
             }
             return count;
         }
