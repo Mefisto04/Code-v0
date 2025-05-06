1bfc62494eee60290498d9e5ee4fc5fc9ad9c1cd
Mefisto04 <kawalemayur30@gmail.com>
2025-05-06 14:52:28 +0530

    Add Build Array from Permutation

--- Build_Array_from_Permutation.cpp
+++ Build_Array_from_Permutation.cpp
@@ -1,10 +1,13 @@
 class Solution {
 public:
     vector<int> buildArray(vector<int>& nums) {
-        vector <int> temp(nums.size(), 0);
-        for (int i = 0 ; i < nums.size() ; i++) {
-            temp[i] = nums[nums[i]];
+        int n = nums.size();
+        for(int i=0; i<n; i++) {
+            nums[i] += n * (nums[nums[i]] % n);       
         }
-        return temp;
+        for(int i=0; i<n; i++) {
+            nums[i] /= n;
+        }
+        return nums;
     }
 };
