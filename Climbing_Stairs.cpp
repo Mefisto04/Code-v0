53cd6f470b51aecba0221c3e6f6ebddd927974a2
Mefisto04 <kawalemayur30@gmail.com>
2025-05-10 16:52:31 +0530

    Add Climbing Stairs

--- Climbing_Stairs.cpp
+++ Climbing_Stairs.cpp
@@ -1,8 +1,9 @@
 class Solution {
 public:
     int climbStairs(int n) {
-        int dp[n+1];
-        for(int i=0;i<=n;i++) dp[i]=0;
+        // int dp[n+1];
+        vector<int>dp(n+1,0);
+        // for(int i=0;i<=n;i++) dp[i]=0;
         dp[1]=1;
         if(n>=2) dp[2]=2;
         for(int i=3;i<=n;i++){
