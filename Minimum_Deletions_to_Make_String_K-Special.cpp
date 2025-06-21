0039ba3de053f87762d29bb0e4cc7920d5c9da96
Mefisto04 <kawalemayur30@gmail.com>
2025-06-21 21:34:10 +0530

    Add Minimum Deletions to Make String K-Special

--- Minimum_Deletions_to_Make_String_K-Special.cpp
+++ Minimum_Deletions_to_Make_String_K-Special.cpp
@@ -1,19 +1,41 @@
+using namespace std;
+
 class Solution {
 public:
-    int minimumDeletions(string word, int k) {
-        unordered_map<char, int> mp;
-        for (char c : word) mp[c]++;
-        vector<int> c;
-        for (auto& pair : mp) c.push_back(pair.second);
-        sort(c.begin(), c.end());
-        int temp = 0;
-        for (int i = 0; i < c.size() - 1; i++) {
-            if (c[i] + k >= c.back()) {
-                temp += c[i];
-                c[i] = 0;
-            }
-        }
-        return temp;
+    int minimumDeletions(const string& word, int k) {
+        array<int, 26> freq{};
+        for (unsigned char c : word)
+            ++freq[c - 'a'];
 
+        vector<int> freqList;
+        freqList.reserve(26);
+        for (int f : freq)
+            if (f > 0)
+                freqList.push_back(f);
+
+        sort(begin(freqList), end(freqList));
+
+        int n = freqList.size(), total = word.size(), removedLeft = 0,
+            inWindowSum = 0, best = INT_MAX, r = 0;
+
+        for (int l = 0; l < n; ++l) {
+            int base = freqList[l];
+            int limit = base + k;
+
+            while (r < n && freqList[r] <= limit)
+                inWindowSum += freqList[r++];
+
+            int rightCount = n - r;
+            int rightSum = total - inWindowSum;
+            int delRight = rightSum - rightCount * limit;
+
+            best = min(best, removedLeft + delRight);
+
+            total -= freqList[l];
+            removedLeft += freqList[l];
+            inWindowSum -= freqList[l];
+        }
+
+        return best;
     }
 };
